ce281dd108cad7ecf3c273d306e7da9e799a3fce
Amos1109 <824203764@qq.com>
2025-03-06 15:33:44 +0800

    fix

--- src/open-r1-multimodal/src/open_r1/utils/math.py
+++ src/open-r1-multimodal/src/open_r1/utils/math.py
@@ -1,20 +1,68 @@
 from math_verify import parse, verify
 def compute_score(solution_str, ground_truth) -> float:
     retval = 0.
+    
     if solution_str == ground_truth:
-        retval = 1.
-        return retval
+        return 1.0 
+
     if float(verify(parse(solution_str), parse(ground_truth))) > 0:
-        retval = 1.
-        return retval
+        return 1.0 
+
     try:
-        if is_equiv(solution_str, ground_truth):
-            retval = 1.
+        answer = solution_str
+        string_in_last_boxed = last_boxed_only_string(solution_str)
+        if string_in_last_boxed is not None:
+            answer = remove_boxed(string_in_last_boxed) 
+
+        if is_equiv(answer, ground_truth):
+            return 1.0 
     except Exception as e:
         print(e)
 
     return retval
 
+
+def remove_boxed(s):
+    if "\\boxed " in s:
+        left = "\\boxed "
+        assert s[:len(left)] == left
+        return s[len(left):]
+
+    left = "\\boxed{"
+
+    assert s[:len(left)] == left
+    assert s[-1] == "}"
+
+    return s[len(left):-1]
+
+def last_boxed_only_string(string):
+    idx = string.rfind("\\boxed")
+    if "\\boxed " in string:
+        return "\\boxed " + string.split("\\boxed ")[-1].split("$")[0]
+    if idx < 0:
+        idx = string.rfind("\\fbox")
+        if idx < 0:
+            return None
+
+    i = idx
+    right_brace_idx = None
+    num_left_braces_open = 0
+    while i < len(string):
+        if string[i] == "{":
+            num_left_braces_open += 1
+        if string[i] == "}":
+            num_left_braces_open -= 1
+            if num_left_braces_open == 0:
+                right_brace_idx = i
+                break
+        i += 1
+
+    if right_brace_idx is None:
+        retval = None
+    else:
+        retval = string[idx:right_brace_idx + 1]
+
+    return retval
 
 # string normalization from https://github.com/EleutherAI/lm-evaluation-harness/blob/master/lm_eval/tasks/hendrycks_math.py
 def is_equiv(str1, str2, verbose=False):
